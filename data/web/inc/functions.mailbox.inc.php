bd74f093f9e56516e40c94e454eab0c33b387bec
none <none@example.org>
2025-01-30 20:35:57 +0100

    testing

--- data/web/inc/functions.mailbox.inc.php
+++ data/web/inc/functions.mailbox.inc.php
@@ -3227,12 +3227,6 @@ function mailbox($_action, $_type, $_data = null, $_extra = null) {
                   ':autoReplyEmailAddresses' => $vacation['autoReplyEmailAddresses'],
                   ':enabled' => $vacation['enabled']
                 ));
-                $_SESSION['return'][] = array(
-                  'type' => 'danger',
-                  'log' => array(__FUNCTION__, $_action, $_type, $_data_log, $_attr),
-                  'msg' => json_encode($_is_now['vacation'])
-                );
-                return false;
               } catch (PDOException $e) {
                 $_SESSION['return'][] = array(
                   'type' => 'danger',
