4bb775087d0444e4bc8f08bfb7f511b8784914b6
none <none@example.org>
2025-01-30 20:26:16 +0100

    testing

--- data/web/inc/functions.mailbox.inc.php
+++ data/web/inc/functions.mailbox.inc.php
@@ -3230,7 +3230,7 @@ function mailbox($_action, $_type, $_data = null, $_extra = null) {
                 $_SESSION['return'][] = array(
                   'type' => 'danger',
                   'log' => array(__FUNCTION__, $_action, $_type, $_data_log, $_attr),
-                  'msg' => $_data['vacation']
+                  'msg' => json_encode($_data['vacation'])
                 );
                 return false;
               } catch (PDOException $e) {
