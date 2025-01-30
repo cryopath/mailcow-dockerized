5bf6d172743b989be710fa7a1436cd96339ac958
none <none@example.org>
2025-01-30 20:29:21 +0100

    testing

--- data/web/inc/functions.mailbox.inc.php
+++ data/web/inc/functions.mailbox.inc.php
@@ -4790,12 +4790,13 @@ function mailbox($_action, $_type, $_data = null, $_extra = null) {
           $mailboxdata['vacation'] = (
             json_decode($row['c_defaults'], true)['Vacation']
           );
-          $_SESSION['return'][] = array(
-            'type' => 'danger',
-            'log' => array(__FUNCTION__, $_action, $_type, $_data_log, $_attr),
-            'msg' => json_encode($mailboxdata['vacation'])
-          );
-          return false;
+
+          //$_SESSION['return'][] = array(
+          //  'type' => 'danger',
+          //  'log' => array(__FUNCTION__, $_action, $_type, $_data_log, $_attr),
+          //  'msg' => json_encode($mailboxdata['vacation'])
+          //);
+          //return false;
 
           if ($mailboxdata['percent_in_use'] === '- ') {
             $mailboxdata['percent_class'] = "info";
